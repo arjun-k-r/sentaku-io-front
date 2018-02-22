d9c84e6a74ad552a956a79e0b3f23e90be3aad75
Mustapha Nezzari <mnezzari@gmail.com>
2018-02-22 20:59:05 +0100

    <chore> Travis config
    Github #14

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './lib/js/src/Main.js',
   output: {
-    path: path.resolve(__dirname, +'/public/js'),
+    path: path.resolve(__dirname, '/public/js'),
     filename: 'bundle.js',
   },
 };
