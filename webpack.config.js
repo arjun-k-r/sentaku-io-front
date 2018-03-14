85c769a60b486f2e7e4c102e966362c988b4be14
Mustapha Nezzari <mnezzari@gmail.com>
2018-03-14 10:38:04 +0100

    Removing parseRating duplicate

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './lib/js/src/App.bs.js',
   output: {
+    publicPath: "/public/",
     path: path.join(__dirname, 'public', 'js'),
     filename: 'bundle.js',
   }
