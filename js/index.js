2451128c63e2010c73f0898ef0ee10212855deea
Mustapha Nezzari <mnezzari@gmail.com>
2018-03-26 23:14:33 +0200

    Test heroku deployment

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// Server for cutii-desktop browser app
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const compression = require('compression');
+const helmet = require('helmet');
+
+const app = express();
+const PORT = process.env.PORT || 3998;
+// variable ci dessous à adapter à votre projet
+const DIST = path.join(__dirname, '..', 'public');
+
+app.enable('trust proxy');
+
+app.use(compression());
+app.disable('x-powered-by');
+app.use(
+  helmet.hsts({
+    // Must be at least 18 weeks to be approved by Google
+    maxAge            : 10886400,
+    // Must be enabled to be approved by Google
+    includeSubDomains : true,
+    preload           : true,
+  })
+);
+
+app.get('*', (req, res, next) => {
+  return res.redirect(308, 'https://' + req.headers.host + req.originalUrl);
+});
+app.use(express.static(DIST));
+
+app.get('*', (req, res) =>
+  fs.createReadStream(path.join(DIST, 'index.html')).pipe(res)
+);
+
+app.listen(PORT, () =>
+  process.stdout.write(`cutii-desktop server started on port ${PORT}\n`)
+);
